809343cf369cda1529b6563f2fc771f8a80b62db
Susana Duque <su.duquem@gmail.com>
2020-09-27 22:21:33 -0500

    Se captura el valor del campo en el componente Welcome

---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
+})
+export class WelcomeComponent implements OnInit {
+  nameAdded = '';
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addName(name, event): void {
+    event.preventDefault();
+    console.log('name typped', name.value);
+    this.nameAdded = name.value;
+  }
+}
